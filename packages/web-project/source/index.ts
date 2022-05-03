7bbde07eb05c2ffedc20b0972496fc8265bb4579
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-03 20:12:22 +0200

    fix: Don't import ignored data

--- packages/web-project/source/index.ts
+++ packages/web-project/source/index.ts
@@ -1,6 +1,3 @@
-import PLANT_LOG_RAW from "../../../plantlog.csv?raw";
-import PLANTS_RAW from "../../../plants.csv?raw";
-
 console.log("Application ready. Using ...");
-console.debug(PLANTS_RAW);
-console.debug(PLANT_LOG_RAW);
+
+export {};
