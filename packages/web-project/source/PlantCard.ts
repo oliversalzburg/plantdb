338aa7e681d91cc97579fbf7757a1abe11f64252
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-07 13:34:39 +0200

    fix: Clean up CSS build

--- packages/web-project/source/PlantCard.ts
+++ packages/web-project/source/PlantCard.ts
@@ -1,38 +1,11 @@
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { DateTime } from "luxon";
+import { style } from "./PlantCard-css";
 
 @customElement("plant-card")
 export class PlantCard extends LitElement {
-  static styles = [
-    css`
-      :host {
-        display: block;
-        margin: var(--block-spacing-vertical) 0;
-        padding: var(--block-spacing-vertical) var(--block-spacing-horizontal);
-        border-radius: var(--border-radius);
-        background: var(--card-background-color);
-        box-shadow: var(--card-box-shadow);
-      }
-      :host > header,
-      :host > footer {
-        margin-right: calc(var(--block-spacing-horizontal) * -1);
-        margin-left: calc(var(--block-spacing-horizontal) * -1);
-        padding: calc(var(--block-spacing-vertical) * 0.66) var(--block-spacing-horizontal);
-        background-color: var(--card-sectionning-background-color);
-      }
-      :host > header {
-        margin-top: calc(var(--block-spacing-vertical) * -1);
-        margin-bottom: var(--block-spacing-vertical);
-        border-bottom: var(--border-width) solid var(--card-border-color);
-      }
-      :host > footer {
-        margin-top: var(--block-spacing-vertical);
-        margin-bottom: calc(var(--block-spacing-vertical) * -1);
-        border-top: var(--border-width) solid var(--card-border-color);
-      }
-    `,
-  ];
+  static styles = [style];
 
   @property()
   plantId = "";
@@ -50,7 +23,8 @@ export class PlantCard extends LitElement {
   lastUpdated = "";
 
   render() {
-    return html`<header><code>${this.plantId}</code></header>
+    return html`<sl-card class="card-basic">
+      <header><code>${this.plantId}</code></header>
       ${this.name}
       <em>${this.kind}</em>
       <footer>
@@ -58,7 +32,8 @@ export class PlantCard extends LitElement {
           <li>Added: ${DateTime.fromJSDate(new Date(this.dateCreated)).toRelative()}</li>
           <li>Last updated: ${DateTime.fromJSDate(new Date(this.lastUpdated)).toRelative()}</li>
         </ul>
-        <button href="#confirm" role="button">Update</button>
-      </footer>`;
+        <sl-button href="#confirm" role="button">Update</sl-button>
+      </footer>
+    </sl-card>`;
   }
 }
--- packages/web-project/source/index-css.js
+++
@@ -1,5 +0,0 @@
-import { css } from "lit-element";
-
-export const style = css`
-  @import url("@picocss/pico/css/pico.min.css");
-`;
