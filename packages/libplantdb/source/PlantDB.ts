0bd61c18cd28ed52fca3491053d34225d51f5c8c
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-18 18:58:05 +0200

    docs: Add missing JSDoc

--- packages/libplantdb/source/PlantDB.ts
+++ packages/libplantdb/source/PlantDB.ts
@@ -3,6 +3,9 @@ import { DatabaseFormat } from "./DatabaseFormat.js";
 import { LogEntry, LogEntrySerialized } from "./LogEntry.js";
 import { Plant, PlantSerialized } from "./Plant.js";
 
+/**
+ * The main entrypoint of a PlantDB data collection.
+ */
 export class PlantDB {
   #config = new DatabaseFormat();
   #plants = new Map<string, Plant>();
@@ -10,34 +13,64 @@ export class PlantDB {
   #entryTypes = new Set<string>();
   #usedProducts = new Set<string>();
 
+  /**
+   * The `DatabaseFormat` used to initialize this database.
+   */
   get config() {
     return this.#config;
   }
 
+  /**
+   * A map of plants that are found within the database.
+   * It maps Plant IDs (like "PID-12") to the actual `Plant`.
+   * If you only want the plants themselves, use `plants.values()`.
+   */
   get plants(): ReadonlyMap<string, Plant> {
     return this.#plants;
   }
 
+  /**
+   * The individual log entries, sorted by their timestamp, starting with the oldest.
+   */
   get log(): ReadonlyArray<LogEntry> {
     return this.#log;
   }
 
+  /**
+   * A cache of all the user-supplied event types in the database.
+   */
   get entryTypes(): ReadonlySet<string> {
     return this.#entryTypes;
   }
 
+  /**
+   * A cache of all the user-supplied products in the database.
+   */
   get usedProducts(): ReadonlySet<string> {
     return this.#usedProducts;
   }
 
+  /**
+   * Returns an empty `PlantDB`.
+   */
   static Empty() {
     return new PlantDB();
   }
 
+  /**
+   * Returns a copy of this `PlantDB`, but with an entirely new log.
+   */
   withNewLog(log: ReadonlyArray<LogEntry>) {
     return PlantDB.fromPlantDB(this, { log });
   }
 
+  /**
+   * Construct a new PlantDB, based on the data in another one, and also optionally override
+   * some of its properties.
+   * @param other The `PlantDB` to initialize the new one from.
+   * @param initializer A hash that contains additional fields to copy into the new instance.
+   * @returns A new `PlantDB` with the provided arguments merged into it.
+   */
   static fromPlantDB(other: PlantDB, initializer?: Partial<PlantDB>) {
     const plantDb = new PlantDB();
     plantDb.#config = initializer?.config
@@ -60,6 +93,13 @@ export class PlantDB {
     return plantDb;
   }
 
+  /**
+   * Constructs a new `PlantDB` based on information found in CSV data.
+   * @param databaseFormat The `DatabaseFormat` that explains how to interpret the data.
+   * @param plantDataRaw The raw plant CSV data.
+   * @param plantLogDataRaw The raw plant log CSV data.
+   * @returns A new `PlantDB` with the information found in the CSV data.
+   */
   static fromCSV(databaseFormat: DatabaseFormat, plantDataRaw: string, plantLogDataRaw: string) {
     const plantDb = new PlantDB();
 
@@ -101,6 +141,13 @@ export class PlantDB {
     return plantDb;
   }
 
+  /**
+   * Constructs a new `PlantDB` based on some plain JavaScript initialization hashes.
+   * @param databaseFormat The `DatabaseFormat` to use for the new database.
+   * @param plants The plants to put into the database.
+   * @param plantLogData The log data to put into the database.
+   * @returns A new `PlantDB` initialized with the given data.
+   */
   static fromJSObjects(
     databaseFormat: DatabaseFormat,
     plants: Array<PlantSerialized>,
