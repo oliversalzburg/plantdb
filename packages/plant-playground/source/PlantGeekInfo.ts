4fd358f518256386f876df28badaa330c6b56177
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-31 22:28:15 +0200

    feat: Experimental API access

--- packages/plant-playground/source/PlantGeekInfo.ts
+++ packages/plant-playground/source/PlantGeekInfo.ts
@@ -3,8 +3,8 @@ import "@shoelace-style/shoelace/dist/components/badge/badge";
 import "@shoelace-style/shoelace/dist/components/button/button";
 import "@shoelace-style/shoelace/dist/components/card/card";
 import "dygraphs/dist/dygraph.css";
-import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { css, html, LitElement, PropertyValueMap } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 
 @customElement("plant-geek-info")
 export class PlantGeekInfo extends LitElement {
@@ -22,12 +22,45 @@ export class PlantGeekInfo extends LitElement {
   @property({ type: Plant })
   plant: Plant | undefined;
 
-  render() {
+  @state()
+  private _plantgeekInfo = "";
+
+  protected updated(
+    _changedProperties: PropertyValueMap<PlantGeekInfo> | Map<PropertyKey, unknown>
+  ): void {
+    if (_changedProperties.has("plant")) {
+      this._updateInfo();
+    }
+  }
+
+  private async _updateInfo() {
     if (!this.plant || !this.plant.plantGeekId) {
       return;
     }
 
-    return html`<a href="https://www.plantgeek.co/plant/${
+    const plantgeekId = Array.isArray(this.plant.plantGeekId)
+      ? this.plant.plantGeekId[0]
+      : this.plant.plantGeekId;
+
+    const geekInfoResponse = await fetch(
+      `https://www.plantgeek.co/plantgeek-api/v1/plant/${plantgeekId}`,
+      {
+        credentials: "omit",
+        headers: {
+          "User-Agent": "plantdb Playground",
+          Accept: "application/json",
+        },
+        method: "GET",
+      }
+    );
+
+    this._plantgeekInfo = (await geekInfoResponse.json()) as string;
+  }
+
+  render() {
+    return html`<sl-textarea .value=${
+      this._plantgeekInfo
+    }></sl-textarea><a href="https://www.plantgeek.co/plant/${
       this.plant.plantGeekId
     }" class="plantgeek-info" target="_blank">Visit ${kindFlatten(
       this.plant.kind
