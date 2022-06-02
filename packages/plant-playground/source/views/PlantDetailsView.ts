7f8447bfb19d2898ee1cbfed397eb88b29df1cd1
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-02 14:23:12 +0200

    feat: Empty state on invalid plant ID

--- packages/plant-playground/source/views/PlantDetailsView.ts
+++ packages/plant-playground/source/views/PlantDetailsView.ts
@@ -1,22 +1,38 @@
 import { Plant } from "@plantdb/libplantdb";
-import { html } from "lit";
+import { t } from "i18next";
+import { css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { View } from "./View";
 
 @customElement("plant-details-view")
 export class PlantDetailsView extends View {
-  static readonly styles = [...View.styles];
+  static readonly styles = [
+    ...View.styles,
+    css`
+      .empty {
+        flex: 1;
+      }
+    `,
+  ];
 
   @property()
   plant: Plant | undefined;
 
   render() {
     return [
-      html`<plant-details
-        .plantStore=${this.plantStore}
-        .plantStoreUi=${this.plantStoreUi}
-        .plant=${this.plant}
-      ></plant-details>`,
+      this.plant
+        ? html`<plant-details
+            .plantStore=${this.plantStore}
+            .plantStoreUi=${this.plantStoreUi}
+            .plant=${this.plant}
+          ></plant-details>`
+        : html`<plant-empty-state class="empty"
+            ><p>${t("empty.plant")}</p>
+
+            <sl-button href="list" variant="primary"
+              >${t("empty.backToPlants")}</sl-button
+            ></plant-empty-state
+          >`,
     ];
   }
 }
