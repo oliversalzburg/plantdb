fc681c82b4e1137080d28f2dfc5ecc6fbc47bfbe
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-04 23:10:15 +0200

    feat: Collapse plant details

--- packages/plant-nursery/source/PlantDetails.ts
+++ packages/plant-nursery/source/PlantDetails.ts
@@ -113,59 +113,63 @@ export class PlantDetails extends LitElement {
 
     const plantDataCsv = this.plantDataAsGraphCSV(this.plant);
 
-    return html`<div class="top">
-        <sl-card @click=${() => this.cx.plantStoreUi.editPlant(this.cx.plant)}>
-          <div slot="header">
-            ${this.plant.location
-              ? html`<sl-tooltip content=${this.plant.location}
-                  ><sl-icon name="geo-alt"></sl-icon
-                ></sl-tooltip>`
-              : undefined}
-            <sl-badge variant="neutral">${this.plant.id}</sl-badge>
-          </div>
-          ${this.plant.name}
-          <br />
-          <small><em>${this.plant.kind}</em></small>
-          <div slot="footer">
-            <small>
-              <ul>
-                <li>
-                  ${t("plant.whenAdded", {
-                    when: this.plant.logEntryOldest
-                      ? DateTime.fromJSDate(this.plant.logEntryOldest.timestamp).toRelative()
-                      : t("never"),
-                  })}
-                </li>
-                <li>
-                  ${t("plant.whenUpdated", {
-                    when: this.plant.logEntryLatest
-                      ? DateTime.fromJSDate(this.plant.logEntryLatest.timestamp).toRelative()
-                      : t("never"),
-                  })}
-                </li>
-              </ul>
-            </small>
-          </div>
-        </sl-card>
-        ${plantDataCsv
-          ? html`<plant-dygraph
-              class="graph"
-              .data=${plantDataCsv}
-              .plant=${this.plant}
-            ></plant-dygraph>`
-          : undefined}
-        ${this.plant.plantGeekId
-          ? html`<plant-geek-info .plant=${this.plant}></plant-geek-info>`
-          : undefined}
-      </div>
+    return html`<sl-details summary=${t("plant.details")} open>
+        <div class="top">
+          <sl-card @click=${() => this.cx.plantStoreUi.editPlant(this.cx.plant)}>
+            <div slot="header">
+              ${this.plant.location
+                ? html`<sl-tooltip content=${this.plant.location}
+                    ><sl-icon name="geo-alt"></sl-icon
+                  ></sl-tooltip>`
+                : undefined}
+              <sl-badge variant="neutral">${this.plant.id}</sl-badge>
+            </div>
+            ${this.plant.name}
+            <br />
+            <small><em>${this.plant.kind}</em></small>
+            <div slot="footer">
+              <small>
+                <ul>
+                  <li>
+                    ${t("plant.whenAdded", {
+                      when: this.plant.logEntryOldest
+                        ? DateTime.fromJSDate(this.plant.logEntryOldest.timestamp).toRelative()
+                        : t("never"),
+                    })}
+                  </li>
+                  <li>
+                    ${t("plant.whenUpdated", {
+                      when: this.plant.logEntryLatest
+                        ? DateTime.fromJSDate(this.plant.logEntryLatest.timestamp).toRelative()
+                        : t("never"),
+                    })}
+                  </li>
+                </ul>
+              </small>
+            </div>
+          </sl-card>
+          ${plantDataCsv
+            ? html`<plant-dygraph
+                class="graph"
+                .data=${plantDataCsv}
+                .plant=${this.plant}
+              ></plant-dygraph>`
+            : undefined}
+          ${this.plant.plantGeekId
+            ? html`<plant-geek-info .plant=${this.plant}></plant-geek-info>`
+            : undefined}
+        </div>
+      </sl-details>
 
-      <plant-log
-        .plantStore=${this.plantStore}
-        .plantStoreUi=${this.plantStoreUi}
-        .log=${this.plant.log}
-        .headerVisible=${false}
-        @plant-edit-entry=${(event: CustomEvent<LogEntry>) =>
-          this.cx.plantStoreUi.editLogEntry(event.detail)}
-      ></plant-log>`;
+      <sl-details summary=${t("plant.log")} open>
+        <plant-log
+          .plantStore=${this.plantStore}
+          .plantStoreUi=${this.plantStoreUi}
+          .log=${this.plant.log}
+          .headerVisible=${false}
+          @plant-edit-entry=${(event: CustomEvent<LogEntry>) =>
+            this.cx.plantStoreUi.editLogEntry(event.detail)}
+        ></plant-log
+      ></sl-details>`;
   }
 }
