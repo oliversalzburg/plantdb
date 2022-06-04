4a06f84119c0bfea8596b7c167b573da134ca910
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-04 20:04:54 +0200

    feat: Android PWA wrapper for Play Store

---
+++ packages/plant-nursery-android/app/src/main/java/io/github/oliversalzburg/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.oliversalzburg.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
