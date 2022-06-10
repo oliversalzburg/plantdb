36f862941e423ca49b1bd62166a7b1425f090c00
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-10 14:26:39 +0200

    fix: <br> used for layout

--- packages/plant-nursery/source/DbConfig.ts
+++ packages/plant-nursery/source/DbConfig.ts
@@ -11,7 +11,9 @@ export class DbConfig extends LitElement {
   static readonly styles = [
     css`
       :host {
-        display: block;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
       }
     `,
   ];
@@ -56,96 +58,98 @@ export class DbConfig extends LitElement {
     ];
     const now = new Date();
 
-    return html`<sl-checkbox
-        id="has-header-row"
-        ?checked=${this.hasHeaderRow}
-        @sl-change=${(event: MouseEvent) => {
-          this.hasHeaderRow = (event.target as SlCheckbox).checked;
-          this.dispatchEvent(
-            new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
-          );
-        }}
-        >${t("dbConfig.hasHeaderRow")}</sl-checkbox
-      ><br /><br />
+    return [
+      html`<sl-checkbox
+          id="has-header-row"
+          ?checked=${this.hasHeaderRow}
+          @sl-change=${(event: MouseEvent) => {
+            this.hasHeaderRow = (event.target as SlCheckbox).checked;
+            this.dispatchEvent(
+              new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
+            );
+          }}
+          >${t("dbConfig.hasHeaderRow")}</sl-checkbox
+        >
 
-      <sl-select
-        id="column-separator"
-        label=${t("dbConfig.columnSeparator")}
-        value=${this.columnSeparator}
-        hoist
-        @sl-change=${(event: MouseEvent) => {
-          this.columnSeparator = (event.target as SlSelect).value as string;
-          this.dispatchEvent(
-            new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
-          );
-        }}
-      >
-        <sl-menu-item value=",">${t("dbConfig.comma")}</sl-menu-item>
-        <sl-menu-item value=";">${t("dbConfig.semicolon")}</sl-menu-item>
-        <sl-menu-item value="&#9;">${t("dbConfig.tab")}</sl-menu-item></sl-select
-      ><br />
+        <sl-select
+          id="column-separator"
+          label=${t("dbConfig.columnSeparator")}
+          value=${this.columnSeparator}
+          hoist
+          @sl-change=${(event: MouseEvent) => {
+            this.columnSeparator = (event.target as SlSelect).value as string;
+            this.dispatchEvent(
+              new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
+            );
+          }}
+        >
+          <sl-menu-item value=",">${t("dbConfig.comma")}</sl-menu-item>
+          <sl-menu-item value=";">${t("dbConfig.semicolon")}</sl-menu-item>
+          <sl-menu-item value="&#9;">${t("dbConfig.tab")}</sl-menu-item></sl-select
+        >
 
-      <sl-select
-        id="decimal-separator"
-        label=${t("dbConfig.decimalSeparator")}
-        value=${this.decimalSeparator}
-        hoist
-        @sl-change=${(event: MouseEvent) => {
-          this.decimalSeparator = (event.target as SlSelect).value as string;
-          this.dispatchEvent(
-            new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
-          );
-        }}
-      >
-        <sl-menu-item value=",">${t("dbConfig.comma")}</sl-menu-item>
-        <sl-menu-item value=".">${t("dbConfig.period")}</sl-menu-item> </sl-select
-      ><br />
+        <sl-select
+          id="decimal-separator"
+          label=${t("dbConfig.decimalSeparator")}
+          value=${this.decimalSeparator}
+          hoist
+          @sl-change=${(event: MouseEvent) => {
+            this.decimalSeparator = (event.target as SlSelect).value as string;
+            this.dispatchEvent(
+              new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
+            );
+          }}
+        >
+          <sl-menu-item value=",">${t("dbConfig.comma")}</sl-menu-item>
+          <sl-menu-item value=".">${t("dbConfig.period")}</sl-menu-item>
+        </sl-select>
 
-      <sl-select
-        id="date-format"
-        label=${t("dbConfig.dateTimeFormat")}
-        value="${this.dateFormat}"
-        hoist
-        @sl-change=${(event: MouseEvent) => {
-          this.dateFormat = (event.target as SlSelect).value as string;
-          this.dispatchEvent(
-            new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
-          );
-        }}
-      >
-        <sl-menu-label>${t("dbConfig.withoutSeconds")}</sl-menu-label>
-        ${dateFormats.map(
-          dateFormat => html`<sl-menu-item value=${dateFormat}
-            >${dateFormat}<small slot="suffix"
-              >${DateTime.fromJSDate(now).toFormat(dateFormat)}</small
-            ></sl-menu-item
-          >`
-        )}
-        <sl-divider></sl-divider
-        ><sl-menu-label>${t("dbConfig.withSeconds")}</sl-menu-label>${dateFormatsSeconds.map(
-          dateFormat => html`<sl-menu-item value=${dateFormat}
-            >${dateFormat}<small slot="suffix"
-              >${DateTime.fromJSDate(now).toFormat(dateFormat)}</small
-            ></sl-menu-item
-          >`
-        )}</sl-select
-      ><br />
+        <sl-select
+          id="date-format"
+          label=${t("dbConfig.dateTimeFormat")}
+          value="${this.dateFormat}"
+          hoist
+          @sl-change=${(event: MouseEvent) => {
+            this.dateFormat = (event.target as SlSelect).value as string;
+            this.dispatchEvent(
+              new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
+            );
+          }}
+        >
+          <sl-menu-label>${t("dbConfig.withoutSeconds")}</sl-menu-label>
+          ${dateFormats.map(
+            dateFormat => html`<sl-menu-item value=${dateFormat}
+              >${dateFormat}<small slot="suffix"
+                >${DateTime.fromJSDate(now).toFormat(dateFormat)}</small
+              ></sl-menu-item
+            >`
+          )}
+          <sl-divider></sl-divider
+          ><sl-menu-label>${t("dbConfig.withSeconds")}</sl-menu-label>${dateFormatsSeconds.map(
+            dateFormat => html`<sl-menu-item value=${dateFormat}
+              >${dateFormat}<small slot="suffix"
+                >${DateTime.fromJSDate(now).toFormat(dateFormat)}</small
+              ></sl-menu-item
+            >`
+          )}</sl-select
+        >
 
-      <sl-select
-        id="timezone"
-        label=${t("dbConfig.timezone")}
-        value="${this.timezone}"
-        hoist
-        @sl-change=${(event: MouseEvent) => {
-          this.timezone = (event.target as SlSelect).value as string;
-          this.dispatchEvent(
-            new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
-          );
-        }}
-      >
-        <sl-menu-item value="Europe/Berlin">Europe/Berlin</sl-menu-item>
-        <sl-menu-item value="utc">UTC</sl-menu-item>
-      </sl-select>`;
+        <sl-select
+          id="timezone"
+          label=${t("dbConfig.timezone")}
+          value="${this.timezone}"
+          hoist
+          @sl-change=${(event: MouseEvent) => {
+            this.timezone = (event.target as SlSelect).value as string;
+            this.dispatchEvent(
+              new CustomEvent("pn-config-changed", { detail: DatabaseFormat.fromJSObject(this) })
+            );
+          }}
+        >
+          <sl-menu-item value="Europe/Berlin">Europe/Berlin</sl-menu-item>
+          <sl-menu-item value="utc">UTC</sl-menu-item>
+        </sl-select>`,
+    ];
   }
 }
 
