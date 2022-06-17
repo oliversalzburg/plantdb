95cdcd4fe069113c8e321bfe92dd1c4c2d3ece9a
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-17 14:07:59 +0200

    feat: Add plant on empty state

--- packages/plant-nursery/source/views/PlantListView.ts
+++ packages/plant-nursery/source/views/PlantListView.ts
@@ -2,6 +2,7 @@ import { Plant } from "@plantdb/libplantdb";
 import { t } from "i18next";
 import { css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { assertExists } from "../tools/Maybe";
 import { View } from "./View";
 
 @customElement("pn-plant-list-view")
@@ -12,11 +13,29 @@ export class PlantListView extends View {
       .empty {
         flex: 1;
       }
+
+      .or {
+        margin: 1rem;
+      }
     `,
   ];
 
   @property({ type: [Plant] })
   plants = new Array<Plant>();
+
+  async createNewPlant() {
+    assertExists(this.plantStore);
+    assertExists(this.plantStoreUi);
+
+    const plant = await this.plantStoreUi.showPlantEditor();
+    if (!plant) {
+      return;
+    }
+
+    console.debug(plant);
+    const newDb = this.plantStore.plantDb.withNewPlant(plant);
+    return this.plantStore.updatePlantDb(newDb);
+  }
 
   render() {
     return [
@@ -30,8 +49,10 @@ export class PlantListView extends View {
         : html`<pn-empty-state class="empty"
             ><p>${t("empty.plants")}</p>
 
-            <sl-button href="import" variant="primary"
-              >${t("empty.importNow")}</sl-button
+            <sl-button href="import" variant="primary">${t("empty.importNow")}</sl-button
+            ><span class="or">${t("empty.or")}</span>
+            <sl-button @click=${() => this.createNewPlant()}
+              >${t("plant.add")}</sl-button
             ></pn-empty-state
           >`,
     ];
