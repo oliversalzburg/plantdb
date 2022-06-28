5701e059f6506e71e620b62d0b7e294459fecb18
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-06-28 08:59:15 +0200

    docs: Don't attempt to build DE docs

--- packages/documentation/.scripts/build.sh
+++ packages/documentation/.scripts/build.sh
@@ -5,5 +5,4 @@ basedir=$(dirname "$self")
 
 cd ${basedir}/../../..
 
-docker run --rm -v ${PWD}:/docs --user="${UID}" ghcr.io/oliversalzburg/mkdocs-material-ex:main build --config-file packages/documentation/de-DE/mkdocs.yml --site-dir=../public/de-DE
 docker run --rm -v ${PWD}:/docs --user="${UID}" ghcr.io/oliversalzburg/mkdocs-material-ex:main build --config-file packages/documentation/en-US/mkdocs.yml --site-dir=../public/en-US
