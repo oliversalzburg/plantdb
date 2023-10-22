27e0b961352a15cab8fedaff42db87e0f58df42f
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-10-22 17:30:42 +0200

    fix: Linter issues in root files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-	printWidth : 100,
-	arrowParens : "avoid",
-	plugins : [
-		require.resolve( "prettier-plugin-organize-imports" ),
-	],
-	overrides : [
-		{
-			files : "*.md",
-			options : {
-				tabWidth : 4
-			}
-		}
-	]
+  printWidth: 100,
+  arrowParens: "avoid",
+  plugins: [require.resolve("prettier-plugin-organize-imports")],
+  overrides: [
+    {
+      files: "*.md",
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };
